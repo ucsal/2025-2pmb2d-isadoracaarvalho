2a1a5ca0edff0f2d5621e84646e5e0409e056e91
Isadora Carvalho <isadora.pereira@ucsal.edu.br>
2025-09-01 20:41:21 -0300

    Refatora para não usar SMTP real

--- src/main/java/br/com/mariojp/solid/dip/EmailNotifier.java
+++ src/main/java/br/com/mariojp/solid/dip/EmailNotifier.java
@@ -1,11 +1,12 @@
 package br.com.mariojp.solid.dip;
 
 public class EmailNotifier {
-	private final SmtpClient smtp = new SmtpClient();
+	private final EmailSender smtp = new SmtpClient();
 
 	public void welcome(User user) {
 		if ("true".equalsIgnoreCase(System.getProperty("DRY_RUN"))) {
 			// Estado inicial: ainda assim usa SMTP real (bug proposital)
+			return;
 		}
 		smtp.send(user.email(), "Bem-vindo", "Olá " + user.name());
 	}
